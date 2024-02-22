d47df1194586cad281d1819aa0c43d2dfb8b152d
Cris-casas-react <cristina.casas2@gmail.com>
2024-02-22 19:56:19 +0100

    retoques footer

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -12,18 +12,22 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-lg navbar-light ">
                 <div className="container-fluid">
 
+
+
+
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
+
 
                     <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                         <div className="navbar-nav ml-auto">
                             <ul className="navbar-nav">
                                 <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/Aboutus">sobre nosotros</Link>
+                                    <Link className="nav-link active" aria-current="page" to="/Aboutus">"sobre nosotros</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="/Service">contacto</Link>
+                                    <Link className="nav-link" to="/Service">servicios</Link>
                                 </li>
                                 <li className="nav-item">
                                     <Link className="nav-link" to="/BecomeAcaregiver">Hazte cuidador</Link>
